dd71b59bcdda1d2977db48de09a38d238d72031d
s1gnature <lamant0486@naver.com>
2021-02-01 21:25:59 +0900

    [18258번] 큐2

---
+++ 18258.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+
+queue = deque([])
+n = int(sys.stdin.readline())
+for _ in range(n):
+    cmd = sys.stdin.readline().split()
+    if 'push' in cmd: cmd, num = cmd[0], cmd[1]
+    else: cmd = cmd[0]
+    
+    if cmd == 'push': queue.append(num)
+    elif cmd == 'pop':
+        if len(queue) == 0: print(-1)
+        else: print(queue.popleft())
+    elif cmd == 'size': print(len(queue))
+    elif cmd == 'empty': print(1 if len(queue) == 0 else 0)
+    elif (cmd == 'front' or cmd == 'back') and (len(queue) == 0): print(-1)
+    elif cmd == 'front': print(queue[0])
+    elif cmd == 'back': print(queue[-1])
+    else:
+        print('elsesesles')
--- 1_test.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-arr = []
-for _ in range(n):
-    x, y = map(int, (input().split()))
-    arr.append([x, y])
-
-arr.sort(key=lambda x: x[0])
-arr.remove(3)
-print(arr)
