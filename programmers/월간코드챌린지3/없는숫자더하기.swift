ac2cf5554294a5d8cce8f9ba689fd7570bb9589e
s1gnature <lamant0486@naver.com>
2021-12-20 16:42:47 +0900

    고득점키트 시작

--- programmers/월간코드챌린지3/없는숫자더하기.swift
+++ programmers/월간코드챌린지3/없는숫자더하기.swift
@@ -1,8 +1,5 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> Int {
-    let arr = [1,2,3,4,5,6,7,8,9,0]
-    return arr.filter(){ !numbers.contains($0) }.reduce(0){ $0 + $1}
+    return numbers.reduce(45){$0-$1}
 }
-
-print(solution([1,2,3,4,6,7,8,0]))
