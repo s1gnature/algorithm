7fe24bbec385afe1877eb375f51e4f07152850de
s1gnature <lamant0486@naver.com>
2021-12-20 17:19:51 +0900

    정수삼각형

---
+++ 고득점kit/dp/정수삼각형.py
@@ -0,0 +1,16 @@
+import sys
+
+def solution(triangle):
+    answer = 0
+    sumOfTriangle = triangle
+    for line in range(1, len(sumOfTriangle)):
+        sumOfTriangle[line][0] += sumOfTriangle[line-1][0]
+        sumOfTriangle[line][-1] += sumOfTriangle[line-1][-1]
+        for index in range(1, len(sumOfTriangle[line])-1):
+            sumOfTriangle[line][index] += max(sumOfTriangle[line-1][index-1], sumOfTriangle[line-1][index])
+    return max(sumOfTriangle[-1])
+
+
+tc1 = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
+tc1_result = 30
+print(solution(tc1))
