1cb0c477e0f557aa443c4f182265a0f03981da4f
s1gnature <lamant0486@naver.com>
2022-01-26 23:45:10 +0900

    [LV2] n^2 배열 자르기

---
+++ programmers/월간코드챌린지3/n^2배열자르기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
+    var res = [Int]()
+    
+    (left...right).forEach {
+        res.append(Int(max(($0 / Int64(n)), ($0 % Int64(n))) + 1))
+    }
+
+    return res
+}
