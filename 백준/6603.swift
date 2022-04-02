0aa9c63ceffaf6d6dee2fcdc17390f3d355cc214
s1gnature <lamant0486@naver.com>
2022-04-03 00:01:26 +0900

    [6603번] 로또

---
+++ 백준/6603.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+while true {
+    var input = readLine()!.split(separator: " ").map { String($0) }
+    if Int(input.removeFirst())! == 0 { break }
+    dfs(input, 0, [String]())
+    print()
+}
+func dfs(_ input: [String], _ currIdx: Int, _ res: [String]) {
+    if res.count == 0 && (currIdx > input.count-6) {
+        return
+    }
+    if res.count == 6 {
+        print(res.joined(separator: " "))
+        return
+    }
+    
+    (currIdx..<input.count).forEach {
+        dfs(input, $0+1, res + [input[$0]])
+    }
+}
