7fba15a1e8906cd5388ae8fe521e82145efdd054
s1gnature <lamant0486@naver.com>
2022-04-09 01:12:51 +0900

    [1016번] 제곱ㄴㄴ수(newSolve)

--- 백준/1016.swift
+++ 백준/1016.swift
@@ -1,29 +1,25 @@
 // [1016번] 제곱ㄴㄴ수
 import Foundation
 
-let input = readLine()!.split(separator: " ").map{ Int(String($0))! }
+let input = readLine()!.split(separator: " ").map{ Int64(String($0))! }
 let (min, max) = (input[0], input[1])
-
-var dic: [Int: Bool] = {
-    var dic = [Int: Bool]()
-    (min...max).forEach {
-        dic[$0] = true
-    }
-    return dic
-}()
-var res = max - min + 1
-var pivot = 2
+var res = Int(max - min + 1)
+var arr = Array(repeating: true, count: res)
+var pivot: Int64 = 2
 while true {
     let powPivot = pivot*pivot
     if powPivot > max { break }
-    let multiplier = (min / powPivot) == 0 ? 1 : (min / powPivot)
-    for divisor in stride(from: powPivot*multiplier, to: max+1, by: +powPivot) {
-        if let value = dic[divisor] {
-            if value {
-                dic[divisor] = false
-                res -= 1
-            }
+    let multiplier = (min/powPivot) == 0 ? 1 : (min/powPivot)
+    var divisor: Int64 = powPivot*multiplier
+    if divisor < min {
+        divisor += powPivot
+    }
+    while divisor <= max {
+        if arr[Int(divisor-min)] {
+            arr[Int(divisor-min)] = false
+            res -= 1
         }
+        divisor += powPivot
     }
     pivot += 1
 }
