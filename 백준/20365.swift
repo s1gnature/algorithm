96a3aa407ab89bde4964c53d2995e4079008c340
s1gnature <lamant0486@naver.com>
2022-04-13 18:04:35 +0900

    [20365번] 블로그

---
+++ 백준/20365.swift
@@ -0,0 +1,20 @@
+// [20365번] 블로그
+import Foundation
+
+let n = Int(readLine()!)!
+let input = readLine()!
+var bCnt = 0
+var rCnt = 0
+var chr = ""
+input.forEach {
+    if String($0) != chr {
+        if $0 == "B" {
+            bCnt += 1
+            chr = "B"
+        } else {
+            rCnt += 1
+            chr = "R"
+        }
+    }
+}
+print(1+min(bCnt, rCnt))
