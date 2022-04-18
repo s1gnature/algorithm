eb67f035486b5eb535855ba501d0626aaaf9453d
s1gnature <lamant0486@naver.com>
2022-04-18 18:10:33 +0900

    최대힙

---
+++ 최대힙.swift
@@ -0,0 +1,76 @@
+import Foundation
+
+// Node = (index, value)
+typealias Node = (Int, Int)
+struct MaxHeap {
+    private var nodes: [Int] = [-1]
+    
+    func print() {
+        var from = 1
+        var to = 2
+        while to < nodes.count {
+            Swift.print(nodes[from..<to])
+            from = to
+            to *= 2
+        }
+        Swift.print(nodes[from...], terminator: "\n\n")
+    }
+    
+    mutating func insert(value: Int) -> Node {
+        nodes.append(value)
+        let node = (nodes.count-1, value)
+        arrangeUp(newNode: node)
+        
+        return node
+    }
+    
+    mutating func remove() -> Int? {
+        if nodes.count == 1 { return nil }
+        nodes.swapAt(1, nodes.count-1)
+        let res = nodes.removeLast()
+        arrangeDown()
+        
+        return res
+    }
+
+    private func parent(from index: Int) -> Node? {
+        if index/2 == 0 { return nil }
+        return (index/2, nodes[index/2])
+    }
+    
+    private func leftChild(from index: Int) -> Node? {
+        if index <= 0 || index*2 >= nodes.count { return nil }
+        return (index*2, nodes[index*2])
+    }
+    
+    private func rightChild(from index: Int) -> Node? {
+        if index <= 0 || index >= nodes.count { return nil }
+        return (index*2+1, nodes[index*2+1])
+    }
+    
+    private mutating func arrangeUp(newNode: Node) {
+        var newNode = newNode
+        while let parent = parent(from: newNode.0) {
+            if parent.1 < newNode.1 {
+                nodes.swapAt(parent.0, newNode.0)
+                newNode.0 = parent.0
+            } else {
+                break
+            }
+        }
+    }
+    
+    private mutating func arrangeDown() {
+        var node = (1, nodes[1])
+        while let left = leftChild(from: node.0) {
+            let right = rightChild(from: node.0) ?? (-1, Int.min)
+            let bigger = left.1 < right.1 ? right : left
+            if node.1 < bigger.1 {
+                nodes.swapAt(node.0, bigger.0)
+                node.0 = bigger.0
+            } else {
+                break
+            }
+        }
+    }
+}
