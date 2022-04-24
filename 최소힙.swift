a4d4dcc4c8ae986c89272003c879e8d7d073cc51
s1gnature <lamant0486@naver.com>
2022-04-24 17:58:01 +0900

    [1927번] 최소힙

---
+++ 최소힙.swift
@@ -0,0 +1,86 @@
+import Foundation
+
+// Node = (index, value)
+typealias Node = (Int, Int)
+struct MinHeap {
+    private var nodes: [Int] = [-1]
+    
+    func print() {
+        var from = 1
+        var to = 2
+        while to < nodes.count {
+            Swift.print(nodes[from..<to])
+            from = to
+            to *= 2
+        }
+        Swift.print(nodes[from...], terminator: "\n\n")
+    }
+    
+    mutating func insert(value: Int) -> Node {
+        nodes.append(value)
+        let node = (nodes.count-1, value)
+        arrangeUp(newNode: node)
+        
+        return node
+    }
+    
+    mutating func remove() -> Int? {
+        if nodes.count == 1 { return nil }
+        nodes.swapAt(1, nodes.count-1)
+        let res = nodes.removeLast()
+        arrangeDown()
+        
+        return res
+    }
+
+    private func parent(from index: Int) -> Node? {
+        if index/2 == 0 { return nil }
+        return (index/2, nodes[index/2])
+    }
+    
+    private func leftChild(from index: Int) -> Node? {
+        if index <= 0 || index*2 >= nodes.count { return nil }
+        return (index*2, nodes[index*2])
+    }
+    
+    private func rightChild(from index: Int) -> Node? {
+        if index <= 0 || index*2+1 >= nodes.count { return nil }
+        return (index*2+1, nodes[index*2+1])
+    }
+    
+    private mutating func arrangeUp(newNode: Node) {
+        var newNode = newNode
+        while let parent = parent(from: newNode.0) {
+            if parent.1 > newNode.1 {
+                nodes.swapAt(parent.0, newNode.0)
+                newNode.0 = parent.0
+            } else {
+                break
+            }
+        }
+    }
+    
+    private mutating func arrangeDown() {
+        if nodes.count == 1 { return }
+        var node = (1, nodes[1])
+        while let left = leftChild(from: node.0) {
+            let right = rightChild(from: node.0) ?? (-1, Int.max)
+            let smaller = left.1 > right.1 ? right : left
+            if node.1 > smaller.1 {
+                nodes.swapAt(node.0, smaller.0)
+                node.0 = smaller.0
+            } else {
+                break
+            }
+        }
+    }
+}
+
+var heap = MinHeap()
+(0..<Int(readLine()!)!).forEach { _ in
+    let n = Int(readLine()!)!
+    if n == 0 { print(heap.remove() ?? 0) }
+    else {
+        heap.insert(value: n)
+    }
+}
